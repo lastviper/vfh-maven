79909df5e37b00e07c6d1743152d8a59bff3868d
Fabian Tietz <fabian.tietz@siemens.com>
2018-12-29 07:55:36 +0100

    # WARNING: head commit changed in the meantime
    initial import

---
+++ src/main/java/de/tietz/swt/vfh_maven/App.java
@@ -0,0 +1,13 @@
+package de.tietz.swt.vfh_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
